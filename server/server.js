42550cb2c239c7724db3d4cfcd5edea720516d02
Sam Davies <sam@razeware.com>
2015-12-08 22:35:50 +0700

    Updated the server to serve the static resources, and started API

--- server/server.js
+++ server/server.js
@@ -6,8 +6,18 @@ const app = express();
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
 var publicPath = path.resolve(__dirname, '..', 'public');
+var distPath = path.resolve(__dirname, '..', 'dist');
 
 app.use(express.static(publicPath));
+app.use(express.static(distPath));
+
+var router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the wenderServe API!'});
+});
+
+app.use('/api', router);
 
 app.listen(port, function() {
 	console.log('Server running on port ' + port);	
