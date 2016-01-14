624b323a4ef6be95f198ba18a2d124772297dc97
Sam Davies <sam@razeware.com>
2016-01-14 16:19:34 +0700

    Removed main.js from the app and added the skeleton to app.js

--- client/tvos/app.js
+++ client/tvos/app.js
@@ -20,4 +20,39 @@
  * THE SOFTWARE.
  */
 
- 
+import Mustache from 'mustache';
+
+App.onLaunch = function(options) {
+  evaluateScripts(options.initialJSDependencies, function(success){
+    if (success) {
+      var resourceLoader = new ResourceLoaderJS(NativeResourceLoader.create());
+      var dataController = new DataController(resourceLoader);
+      var searchHandler = new SearchHandler(resourceLoader, dataController);
+      var presenter = new Presenter(resourceLoader, searchHandler);
+      var eventHandler = new EventHandler(presenter, dataController);
+
+      presenter.present("rootMenu.tvml", null, "push", eventHandler);
+    } else {
+      var alert = createAlert("Evaluate Scripts Error", "There was an error attempting to evaluate the external JavaScript files.");
+      navigationDocument.presentModal(alert);
+
+      throw ("Playback Example: unable to evaluate scripts.");
+    }
+  });
+  
+};
+
+var createAlert = function(title, description) {  
+  var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+      <alertTemplate>
+        <title>${title}</title>
+        <description>${description}</description>
+      </alertTemplate>
+    </document>`
+
+  var parser = new DOMParser();
+  var alertDoc = parser.parseFromString(alertString, "application/xml");
+  return alertDoc
+};
+
